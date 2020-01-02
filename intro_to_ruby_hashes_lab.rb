19543da9d344e3387683a701baef1d0c125b3cd9
Daryl Corbin <corbindac@gmail.com>
2020-01-02 00:38:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ end
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
   key = {:name_2 => "Jay-Z"}
-  value = {:name => "Gradd Hopper"}
+  value = {:name => "Grace Hopper"}
 end
 
 def read_from_hash(hash, key)
